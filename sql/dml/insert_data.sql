a1df772f09c391f72bad79d91be52db8e7fe1d82
William-Mahnke <william.mahnke@gmail.com>
2026-07-09 16:58:34 -0700

    tweak insert data

--- sql/dml/insert_data.sql
+++ sql/dml/insert_data.sql
@@ -1,12 +1,12 @@
-\set ON_ERROR_STOP on
+BEGIN;
 
 -- Clear existing data (including reseting SERIAL for city)
 TRUNCATE weather.statistics;
 TRUNCATE weather.city RESTART IDENTITY CASCADE;
 
 -- Popualte database with processed CSVs
-\copy weather.city(city_id, city_name, latitude, longitude, elevation, timezone)
-FROM 'data/processed/cities.csv'
+COPY weather.city(city_id, city_name, latitude, longitude, elevation, timezone)
+FROM '/tmp/cities.csv'
 WITH (FORMAT csv, HEADER true);
 
 -- Sync city_id after load
@@ -15,22 +15,9 @@ SELECT setval(
     COALESCE((SELECT MAX(city_id) FROM weather.city), 1)
 );
 
-\copy weather.statistics(city_id, date, mean_temp_f, max_temp_f, min_temp_f, total_rain_in,
+COPY weather.statistics(city_id, date, mean_temp_f, max_temp_f, min_temp_f, total_rain_in,
                         total_snow_in, max_wind_speed_mph, daylight_duration_s)
-FROM 'data/processed/weather.csv'
+FROM '/tmp/weather.csv'
 WITH (FORMAT csv, HEADER true);
 
--- Example insert pattern (one city row):
-/*
-INSERT INTO weather.city
-    (city_id, city_name, latitude, longitude, elevation, timezone)
-VALUES
-     (1, 'San Jose', 37.293495, -121.870026, 73.0, 'America/Los_Angeles');
-
-Example DML pattern (one weather row):
-INSERT INTO weather.statistics
-     (city_id, date, mean_temp_f, max_temp_f, min_temp_f,
-      total_rain_in, total_snow_in, max_wind_speed_mph, daylight_duration_s)
- VALUES
-     (1, '2023-01-01', 49.2, 57.4, 42.4, 0.0, 0.0, 12.7, 34732.49);
-*/
+COMMIT;
